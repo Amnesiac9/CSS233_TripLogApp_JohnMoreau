0ad7f459a5a3c629f9224fe8104dbe0e27590b73
John <johnmoreau19@gmail.com>
2023-11-19 12:41:34 -0800

    Add project files.

---
+++ CSS233_TripLogApp_JohnMoreau/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using CSS233_TripLogApp_JohnMoreau.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CSS233_TripLogApp_JohnMoreau.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSS233_TripLogApp_JohnMoreau/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CSS233_TripLogApp_JohnMoreau.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

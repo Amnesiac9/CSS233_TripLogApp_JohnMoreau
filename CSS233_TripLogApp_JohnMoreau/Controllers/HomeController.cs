d4afe0c498524724756bff76c694dba369cc9197
John <johnmoreau19@gmail.com>
2023-11-19 21:53:44 -0800

    Comments

--- CSS233_TripLogApp_JohnMoreau/Controllers/HomeController.cs
+++ CSS233_TripLogApp_JohnMoreau/Controllers/HomeController.cs
@@ -14,8 +14,10 @@ namespace CSS233_TripLogApp_JohnMoreau.Controllers
         public IActionResult Index(string sortBy, string sortOrder)
         {
 
+            // Clear Temp Data
             TempData.Clear();
 
+            // Set Context
             var trips = Context.Trips;
 
             // Return early if SortOrder is empty to increase performance.
